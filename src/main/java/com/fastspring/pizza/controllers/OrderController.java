84872e819617f8a15add5ac3d5145e8237a51940
daniel <dnl.molina@gmail.com>
2023-11-29 19:29:23 -0500

    change api url

--- src/main/java/com/fastspring/pizza/controllers/OrderController.java
+++ src/main/java/com/fastspring/pizza/controllers/OrderController.java
@@ -8,26 +8,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.ui.Model;
-import com.fastspring.pizza.services.*;
-import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
-import java.util.Collection;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/order")
